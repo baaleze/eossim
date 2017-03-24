fca97a986cc4388bf6651c2b7541c8da615a323d
fdroumaguet <fdroumaguet@itlink.fr>
2017-03-24 16:08:50 +0100

    first graphics utility methods

--- core/src/fr/vahren/eossim/graphics/ESGraphics.java
+++ core/src/fr/vahren/eossim/graphics/ESGraphics.java
@@ -1,14 +1,29 @@
 package fr.vahren.eossim.graphics;
 
 import squidpony.squidgrid.gui.gdx.SquidLayers;
+import squidpony.squidmath.Coord;
 
 /**
  * Created by fdroumaguet on 24/03/2017.
+ * Graphics utility class
  */
 public class ESGraphics {
     private final SquidLayers display;
 
     public ESGraphics(SquidLayers display) {
         this.display = display;
+    }
+
+    /**
+     * Add text in bottom status window. Text is formatted with tags for color.<br>
+     *     Ex : This is a @red@red word @black@ coming back to normal.
+     * @param formattedText Texte formatté
+     */
+    public void addStatusText(String formattedText) {
+
+    }
+
+    public void newTextBox(String formattedText, Coord position){
+
     }
 }
